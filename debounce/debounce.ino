9ed448a950e6a53af0b9a27c1f8a7ff1600f46a6
jmsugarman <jmsugarman@URB218-27.seasad.wustl.edu>
2017-10-09 14:23:52 -0500

    Done! :)

---
+++ debounce/debounce.ino
@@ -0,0 +1,50 @@
+const int buttonPin = 2;
+volatile int pressed = 0;
+long timer = 0;
+const long INTERVAL = 50;
+unsigned long lastDebounceTime = 0;
+
+int buttonState;
+int lastButtonState = HIGH;
+
+
+void buttonPressed() {
+  int reading = digitalRead(buttonPin);
+  /*
+  
+  
+    if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+   */
+  
+  if (millis() > timer){
+    if (reading != buttonState){
+            buttonState = reading;
+
+            if (buttonState == LOW) {
+              Serial.println("pressed");
+              timer = millis() + INTERVAL;
+            }
+    }
+  }
+
+  lastButtonState = reading;
+}
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+
+
+
+  // Interrupts can happen on "edges" of signals.  
+  // Three edge types are supported: CHANGE, RISING, and FALLING 
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonPressed, CHANGE);
+  Serial.begin(9600);
+}
+
+void loop() {
+  delay(1000);
+  Serial.println("1 second");
+  }
+
